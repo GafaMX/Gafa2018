6c0f30fc29c940f73235e05c08917ed5edb0bc2b
Unknown <christian@gafa.mx>
2019-02-13 15:59:39 -0600

    cambios

--- wp-content/themes/norebro-child/module-clients.php
+++ wp-content/themes/norebro-child/module-clients.php
@@ -6,7 +6,7 @@
 <div class="gafa__clientes">
 	<div class="container">
 		<div class="row gafa__clientes--items">
-			<div class="col-6 col-md-3">
+			<div class="col">
 				<div class="tooltip" data-tooltip="Google" data-tooltip-desc="UI/UX/Branding" data-tooltip-type="primary">
 					<div class="gafa__clientes--item gafa__effects--glitch">
 						<img class="gafa__clientes--item--img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-01.svg" />
@@ -15,59 +15,113 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-6 col-md-3">
+			<div class="col">
 				<div class="gafa__clientes--item gafa__effects--glitch">
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo1.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo1.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo1.png" />
 				</div>
 			</div>
-			<div class="col-6 col-md-3">
+			<div class="col">
 				<div class="gafa__clientes--item gafa__effects--glitch">
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo2.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo2.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo2.png" />
 				</div>
 			</div>
-			<div class="col-6 col-md-3">
+			<div class="col">
 				<div class="gafa__clientes--item gafa__effects--glitch">
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo3.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo3.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo3.png" />
 				</div>
 			</div>
-			<div class="col-6 col-md-3">
+			<div class="col">
 				<div class="gafa__clientes--item gafa__effects--glitch">
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo4.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo4.png" />
 					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo4.png" />
 				</div>
 			</div>
-			<div class="col-6 col-md-3">
-				<div class="gafa__clientes--item gafa__effects--glitch">
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
-				</div>
-			</div>
-			<div class="col-6 col-md-3">
-				<div class="gafa__clientes--item gafa__effects--glitch">
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
-				</div>
-			</div>
-			<div class="col-6 col-md-3">
-				<div class="gafa__clientes--item gafa__effects--glitch">
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
-					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
-				</div>
-			</div>
-
-
-
 		</div>
+		<div class="row gafa__clientes--items">
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo5.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo6.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo7.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo8.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo8.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo8.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo9.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo9.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo9.png" />
+				</div>
+			</div>
+		</div>
+
+		<div class="row gafa__clientes--items">
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo10.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo10.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo10.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo11.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo11.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo11.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo12.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo12.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo12.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo13.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo13.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo13.png" />
+				</div>
+			</div>
+			<div class="col">
+				<div class="gafa__clientes--item gafa__effects--glitch">
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo14.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo14.png" />
+					<img class="gafa__clientes--item--img" src="https://chapultepec.org.mx/wp-content/uploads/2018/12/logo14.png" />
+				</div>
+			</div>
+		</div>
+
+
+
 	</div>
 	<div class="tooltip-window"></div>
 </div>
--- wp-content/themes/norebro/parts/elements/header-menu-hamburger.php
+++ wp-content/themes/norebro/parts/elements/header-menu-hamburger.php
@@ -5,7 +5,10 @@
 
 <!-- Fullscreen -->
 <?php if ( $menu_type == 'hamburger' ) : ?>
-	<div class="hamburger-menu" id="hamburger-fullscreen-menu">
+	<div class="gafa__logo-side">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo--gafa.svg" />
+	</div>
+	<div class="hamburger-menu xx" id="hamburger-fullscreen-menu">
 		<a class="hamburger" aria-controls="site-navigation" aria-expanded="false"></a>
 	</div>
 <?php endif; ?>
