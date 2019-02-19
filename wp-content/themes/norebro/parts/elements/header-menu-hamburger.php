0aba6205caaaa1eba46f205fa797407277d8d0d8
Unknown <christian@gafa.mx>
2019-02-19 12:07:37 -0600

    logo link home

--- wp-content/themes/norebro/parts/elements/header-menu-hamburger.php
+++ wp-content/themes/norebro/parts/elements/header-menu-hamburger.php
@@ -6,7 +6,7 @@
 <!-- Fullscreen -->
 <?php if ( $menu_type == 'hamburger' ) : ?>
 	<div class="gafa__logo-side">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo--gafa.svg" />
+		<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo--gafa.svg" /></a>
 	</div>
 	<div class="hamburger-menu xx" id="hamburger-fullscreen-menu">
 		<a class="hamburger" aria-controls="site-navigation" aria-expanded="false"></a>
